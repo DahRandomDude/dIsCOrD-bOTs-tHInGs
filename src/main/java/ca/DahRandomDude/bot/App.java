7323e9203f19dfde51b06ca1e5ed99049d3c1c64
DahRandomDude <51520831+DahRandomDude@users.noreply.github.com>
2020-03-07 16:04:10 -0800

    Done Installing

---
+++ src/main/java/ca/DahRandomDude/bot/App.java
@@ -0,0 +1,13 @@
+package ca.DahRandomDude.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
